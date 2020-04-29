faad1becfeaa732a082c71f955acbc1971d82c44
Ivan <ivan@Ivans-MacBook-Pro-3.local>
2020-04-29 17:25:21 +0300

    Home task #5, v.1

---
+++ projectdjango/urls.py
@@ -0,0 +1,34 @@
+"""projectdjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from test_app.views import hello, gen_password, get_unique_firstnames, get_filtered_by_state_and_city, get_revenue
+
+urlpatterns = [
+    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('hello/', hello),
+    path('gen-password', gen_password),
+    path('get-unique-firstnames', get_unique_firstnames),
+    path('filter-by-state-and-city', get_filtered_by_state_and_city),
+    path('get-revenue', get_revenue),
+
+
+
+
+
+]
---
+++ test_app/views.py
@@ -0,0 +1,58 @@
+import os
+import random
+import sqlite3
+import string
+
+from django.shortcuts import render
+from django.http import HttpResponse, request
+
+#Constants
+ROOT_DIR = "/Users/ivan/PythonProjects/ProjectDjango/test_app"
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello")
+
+def gen_password(request):
+    length = request.GET['length']
+    try:
+        val = int(length)
+        if (val > 0):
+            if (8 <= val <= 24):
+                return HttpResponse(''.join(
+                    [
+                        random.choice(string.ascii_lowercase)
+                        for _ in range(val)
+                    ])
+                )
+            else:
+                return HttpResponse("Length should be in the range from 8 to 24.")
+        else:
+            return HttpResponse("Length should be bigger than 0.")
+    except ValueError:
+        return HttpResponse("Length should be a number!")
+
+def get_unique_firstnames(request):
+    query = 'SELECT DISTINCT FirstName FROM customers'
+    records = execute_query(query)
+    return HttpResponse(str(records))
+
+def get_filtered_by_state_and_city(request):
+    state = request.GET['state']
+    city = request.GET['city']
+    query = f'SELECT * FROM customers WHERE State = "{state}" AND City = "{city}"'
+    records = execute_query(query)
+    return HttpResponse(str(records))
+
+def get_revenue(request):
+    query = 'SELECT SUM(UnitPrice * Quantity) FROM invoice_items'
+    records = execute_query(query)
+    return HttpResponse(str(records))
+
+def execute_query(query):
+    db_path = os.path.join(ROOT_DIR, 'chinook.db')
+    print(db_path)
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    cur.execute(query)
+    records = cur.fetchall()
+    return records
